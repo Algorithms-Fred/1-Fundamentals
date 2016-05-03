a31342d256cac5d8d246a81b23e6cd26aed56901
FredFang <530354035@qq.com>
2016-05-03 23:01:47 +0800

    Create Ex_1_2_06.java

---
+++ 1-Fundamentals/1-2-Data-Abstraction/Ex_1_2_06.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Demo2 {
+	
+	public static boolean isCircularRotation(String s, String t) {
+		
+		return s.length() == t.length() && (s + s).contains(t); 
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		String s1 = "TGACGAC";
+		String s2 = "ACTGACG";
+		
+		StdOut.println(isCircularRotation(s1, s2));
+		
+	}
+	
+}
