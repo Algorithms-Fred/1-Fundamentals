c2bd533c01b1c62bfefe8bc47da6802a93233e04
FredFang <530354035@qq.com>
2016-05-05 13:40:35 +0800

    Create Ex_1_2_14.java

---
+++ 1-Fundamentals/1-2-Data-Abstraction/Ex_1_2_14.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Transaction {
+
+	private final String name;
+	private final Date date;
+	private final double amount;
+	
+	public Transaction(String n, Date d, double a) {
+		name = n;
+		date = d;
+		amount = a;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public Date date() {
+		return date;
+	}
+	
+	public double amount() {
+		return amount;
+	}
+	
+	public String toString() {
+		return name() + "\n" + date() + "\n" + amount();
+	}
+	
+	public boolean equals(Object x) {
+		if (this == x) return true;
+		if (x == null) return false;
+		if (this.getClass() != x.getClass()) return false;
+		Transaction that = (Transaction) x;
+		if (this.name != that.name) return false;
+		if (this.date != that.date) return false;
+		if (this.amount != that.amount) return false;
+		return true;
+	}
+}
